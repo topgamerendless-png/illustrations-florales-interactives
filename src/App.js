c84b1a9f371d92ad8c8afef5453b23abc114a73f
topgamerendless-png <wh1txpeif@hotmail.com>
2025-08-29 21:42:10 -0700

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Créer vos illustrations florales</h1>
+      <p>Choisissez vos couleurs et personnalisez vos formes !</p>
+    </div>
+  );
+}
+
+export default App;
