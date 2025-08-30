95409a254b6dd0366fc0043d5357516a7a083483
topgamerendless-png <wh1txpeif@hotmail.com>
2025-08-29 21:45:33 -0700

    Add comments to webpack configuration for clarity

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 
 module.exports = {
+  // Entry point for the application
   entry: './src/index.js',
   output: {
+    // Output bundle file name and path
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
   },
   devServer: {
+    // Configure the development server
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 3000
@@ -15,11 +18,13 @@ module.exports = {
   module: {
     rules: [
       {
+        // Apply Babel loader for JS files
         test: /\.js$/, 
         exclude: /node_modules/, 
         use: {
           loader: 'babel-loader',
           options: {
+            // Babel presets for ES and React
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
